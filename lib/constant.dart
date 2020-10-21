2461e2dae2a419c4f9eff80721b8ea4ae809e0b4
M.Sherazi <sherazahmad720@gmail.com>
2020-10-21 08:44:50 +0500

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kBackground = Color(0xFFF4FAFB);
+
+const kButtonBackground = Color(0xFF7ABDD7);
+const kCancelButtonBackground = Color(0xFF9FC3CF);
+const kTextColorBlack = Color(0xFF2C5364);
+const kTextColorWhite = Color(0xFFF4FAFB);
+const kTextColorDarkBlue = Color(0xFF2C7493);
+const kButtonConverterBackgroud = Colors.grey;
+
+const typeTextStyle = TextStyle(
+  fontSize: 28,
+  color: kButtonBackground,
+);
+
+const kResultTextStyle = TextStyle(
+    fontSize: 40, color: kTextColorBlack, fontWeight: FontWeight.w500);
---
+++ lib/widgets/button.dart
@@ -0,0 +1,72 @@
+import 'package:calculator/constant.dart';
+import 'package:flutter/material.dart';
+
+class CalculatorButton extends StatelessWidget {
+  const CalculatorButton({
+    this.backgroundColor = Colors.transparent,
+    this.textColor = kTextColorBlack,
+    @required this.text,
+    @required this.press,
+  });
+
+  final Color backgroundColor;
+  final Color textColor;
+  final String text;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: backgroundColor,
+        child: FlatButton(
+          color: backgroundColor,
+          padding: EdgeInsets.all(30),
+          onPressed: press,
+          child: Text(
+            "$text",
+            style: TextStyle(
+              color: textColor,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ConverterButton extends StatelessWidget {
+  const ConverterButton({
+    this.backgroundColor = Colors.transparent,
+    this.textColor = kTextColorBlack,
+    @required this.text,
+    @required this.press,
+  });
+
+  final Color backgroundColor;
+  final Color textColor;
+  final String text;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: backgroundColor,
+        child: FlatButton(
+          color: backgroundColor,
+          padding: EdgeInsets.all(10),
+          onPressed: press,
+          child: Text(
+            "$text",
+            style: TextStyle(
+              color: textColor,
+              fontSize: 15,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
